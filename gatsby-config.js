17bd8ea1b833451e17b4b3ee8c4ee8563953baaf
marb <marb61a@gmail.com>
2019-10-07 21:12:22 +0100

    First commit & push to netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A restaurant website built with Gatsby`,
+    author: `Martin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
