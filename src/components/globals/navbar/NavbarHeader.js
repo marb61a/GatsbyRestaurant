058f3e35c08b9a0eb3b05ae3ea9153d0f2f62204
marb <marb61a@gmail.com>
2019-10-13 20:26:10 +0100

    added styling and functionality to navbar header component

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
-
 import {FaAlignRight} from "react-icons"
 import styled from "styled-components"
+import logo from ""
 
 export default class NavbarHeader extends Component {
     render() {
         const {handleNavbar} = this.props;
         return (
-            <div>
-                
-            </div>
+            <HeaderWrapper>
+                <Link to="/">
+                    <img src={logo} alt="Company name"/>
+                </Link>
+                <FaAlignRight 
+                    className="toggle-icon"
+                    onClick={()=>{handleNavbar()}}
+                >
+                </FaAlignRight>
+            </HeaderWrapper>
         )
     }
 }
+
+const HeaderWrapper = styled.div`
+    padding: 0.4rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .toggle-icon{
+        font-size: 1.75rem;
+        color: yellow;
+        cursor: pointer;
+    }
+    @media(min-width:768px){
+        .toggle-icon{
+            display: none;
+        }
+    }
+`;
