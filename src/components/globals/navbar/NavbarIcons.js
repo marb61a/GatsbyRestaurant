7b1ab24146bd231162fc0896afc7ed9d2f83a05d
marb <marb61a@gmail.com>
2019-10-20 19:35:28 +0100

    updated navbaricons component

--- src/components/globals/navbar/NavbarIcons.js
+++ src/components/globals/navbar/NavbarIcons.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react'
+import styled from "styled-components"
+import {FaInstagram, FaTwitter, FaFacebook} from 'react-icons/fa'
+
+import {styles} from "../../../utils"
 
 export default class NavbarIcons extends Component {
+    state = {
+        icons: [
+            {
+                id : 1,
+                icon: < FaFacebook className="icon facebook-icon"/>,
+                path: 'https://www.facebook.com'
+            },
+            {
+                id : 2,
+                icon: < FaTwitter className="icon twitter-icon"/>,
+                path: 'https://www.twitter.com'
+            },
+            {
+                id : 3,
+                icon: < FaInstagram className="icon instagram-icon"/>,
+                path: 'https://www.instagram.com'
+            },
+        ]
+    }
+
     render() {
         return (
             <div>
--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -67,4 +67,18 @@ const LinkWrapper = styled.div`
             padding: 0.5rem 1rem 0.5rem 1.3rem
         }
     }
+    height: ${props => (
+        props.open ? '152px' : '0px'
+    )};
+    overflow: hidden;
+    ${styles.transObject({time: '1s'})}
+    @media(min-width: 768px){
+        height: auto;
+        display: flex;
+        margin: 0 auto;
+        .nav-link: hover{
+            background: ${styles.colors.mainWhite};
+            padding: 0.5rem 1rem 0.5rem 1rem
+        }
+    }
 `;
