3923e81b54051825eff3c13b61cd8e7900413a28
marb <marb61a@gmail.com>
2019-10-28 20:17:30 +0000

    added initial Banner util file and updated QuickInfo global

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -10,9 +10,15 @@ export default class QuickInfo extends Component {
             <Section>
                 <Title message='Let us tell you' title='Our Mission'/>
                 <QuickInfoWrapper>
-                    <p className='text'>
-
+                    <p className="text">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde
+                        blanditiis dolorum quae doloremque molestias expedita, eum voluptas
+                        distinctio! Molestiae fuga temporibus nemo non vel mollitia nesciunt
+                        quaerat facere voluptate earum.
                     </p>
+                    <Link to="/about/" style={{ textDecoration: 'none' }}>
+                        <SectionButton style={{ margin: '2rem auto' }}>about</SectionButton>
+                    </Link>
                 </QuickInfoWrapper>
             </Section>
         )
@@ -26,5 +32,11 @@ const QuickInfoWrapper = styled.div`
         line-height: 2em;
         color: ${styles.colors.mainGrey};
         word-spacing: 0.2rem;
+    }
+    @media (min-width: 768px) {
+        width: 70%;
+    }
+    @media (min-width: 992px) {
+        width: 60%;
     }
 `;
---
+++ src/utils/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { styles } from '../utils'
+
+export default class Banner extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
