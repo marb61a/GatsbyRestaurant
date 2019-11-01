fa893266fdceca92301035469ed8da393f3c5487
marb <marb61a@gmail.com>
2019-11-01 19:46:45 +0000

    updated Menu HomePageComponent file

--- src/components/HomePageComponents/Menu.js
+++ src/components/HomePageComponents/Menu.js
@@ -9,7 +9,23 @@ import Product from './Product'
 export default function Menu() {
     return(
         <Section>
+            <Title title="featured items" message="little taste" />
+            <ProductList>
+                <StaticQuery 
+                    query={graphql`{
 
+                    }`
+                    }
+                    render={
+                        data => {
+                            const { edges: sweets } = data.items;
+                            return sweets.map(item => {
+                                return <Product key={item.node.id} product={item.node} />
+                            })
+                        }
+                    }
+                />
+            </ProductList>
         </Section>
     );
 }
@@ -17,5 +33,18 @@ export default function Menu() {
 export const ProductList = styled.div`
     margin: 3rem 0;
     display: grid;
-    
+    grid-auto-columns: 100%;
+    grid-row-gap: 3rem;
+    @media (min-width: 576px) {
+        display: grid;
+        grid-template-columns: 95%;
+    }
+    @media (min-width: 776px) {
+        grid-template-columns: 80%;
+        justify-content: center;
+    }
+    @media (min-width: 992px) {
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 2rem;
+    }
 `;
