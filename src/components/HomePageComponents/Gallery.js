64d32ec00499be1e3a3e5b7e7a131eb857e18341
marb <marb61a@gmail.com>
2019-11-01 19:31:50 +0000

    updated Gallery HomePageComponent file

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -9,10 +9,24 @@ export default function Gallery() {
     return(
         <StaticQuery 
             query={
-
+                graphql`
+                
+                `
             }
             render={
-                
+                data => {
+                    const img1 = data.img1.childImageSharp.fluid
+                    const img2 = data.img2.childImageSharp.fluid
+                    const img3 = data.img3.childImageSharp.fluid
+
+                    return(
+                        <Section>
+                            <GalleryWrapper>
+
+                            </GalleryWrapper>
+                        </Section>
+                    )
+                }
             }
         />
     )
@@ -27,5 +41,18 @@ const GalleryWrapper = styled.div`
     }
     .info {
         position: absolute;
+        top: 0;
+        left: 0;
+        background: ${styles.colors.mainYellow};
+        padding: 0.1rem 0.3rem;
+        text-transform: capitalize;
     }
+    @media (min-width: 576px) {
+        grid-template-columns: 1fr 1fr;
+        grid-column-gap: 1rem;
+    }
+    @media (min-width: 768px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+    
 `;
