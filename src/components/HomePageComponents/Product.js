06ccfae13f0d5fe4c7d5afcc376aca9c5c51323e
marb <marb61a@gmail.com>
2019-11-08 12:51:41 +0000

    updated Product homepage component

--- src/components/HomePageComponents/Product.js
+++ src/components/HomePageComponents/Product.js
@@ -5,10 +5,21 @@ import Img from 'gatsby-image'
 import { styles } from '../../utils'
 
 export default function Product({ product }) {
+    const { name, price, ingredients } = product;
+    const { fixed } = product.img;
+
     return(
         <ProductWrapper>
-            <div className="text">
+            <Img fixed={fixed} className="img"/>
 
+            <div className="text">
+                <div>
+                    <h3 className="name">{name}</h3>
+                    <h3 className="price">${price}</h3>
+                </div>
+                <p className="info">
+                    {ingredients}
+                </p>
             </div>
         </ProductWrapper>
     )
@@ -20,5 +31,21 @@ export const ProductWrapper = styled.div`
         grid-template-columns: auto 1fr;
         grid-column-gap: 1rem;
     }
-
+    .img {
+        border-radius: 0.5rem;
+    }
+    .product-content {
+        display: flex;
+        justify-content: space-between;
+        font-size: 1.4rem;
+        text-transform: uppercase;
+    }
+    .name {
+        color: ${styles.colors.mainGrey};
+        margin-top: 0.5rem;
+    }
+    .price {
+        color: ${styles.colors.mainYellow};
+        margin-top: 0.5rem;
+    }
 `;
